7accc36f94a0d1bcfb7f407a810bf8bc80f82b60
CodingLooHee <pitchanun2003@gmail.com>
2021-12-07 21:17:23 +0700

    Move unit to a new package and add unit converter

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import enum
 import os
 from pathlib import *
 import numpy as np
+
+# pycalc package
 from pycalc.dna import *
 from pycalc.n_base import *
+from pycalc.units import *
 
 
 # *Additional import
@@ -43,27 +46,6 @@ RAD = pi / 180
 
 # Path
 CWD = Path.cwd()
-# Unit
-yotta = 1e24
-zetta = 1e21
-exa = 1e18
-peta = 1e15
-tera = 1e12
-giga = 1e9
-mega = 1e6
-kilo = 1e3
-hecto = 1e2
-deca = 1e1
-deci = 1e-1
-centi = 1e-2
-milli = 1e-3
-micro = 1e-6
-nano = 1e-9
-pico = 1e-12
-femto = 1e-15
-atto = 1e-18
-zepto = 1e-21
-yocto = 1e-24
 # Physic
 ke = 9e9        # K constant (electric)
 qe = -1.6e-19   # Electron charge (coulomb)
---
+++ pycalc/units.py
@@ -0,0 +1,44 @@
+# Unit
+yotta = 1e24
+zetta = 1e21
+exa = 1e18
+peta = 1e15
+tera = 1e12
+giga = 1e9
+mega = 1e6
+kilo = 1e3
+hecto = 1e2
+deca = 1e1
+deci = 1e-1
+centi = 1e-2
+milli = 1e-3
+micro = 1e-6
+nano = 1e-9
+pico = 1e-12
+femto = 1e-15
+atto = 1e-18
+zepto = 1e-21
+yocto = 1e-24
+
+
+# From no unit to unit
+def to_yotta(x):return x/yotta
+def to_zetta(x):return x/zetta
+def to_exa(x):return x/exa
+def to_peta(x):return x/peta
+def to_tera(x):return x/tera
+def to_giga(x):return x/giga
+def to_mega(x):return x/mega
+def to_kilo(x):return x/kilo
+def to_hecto(x):return x/hecto
+def to_deca(x):return x/deca
+def to_deci(x):return x/deci
+def to_centi(x):return x/centi
+def to_milli(x):return x/milli
+def to_micro(x):return x/micro
+def to_nano(x):return x/nano
+def to_pico(x):return x/pico
+def to_femto(x):return x/femto
+def to_atto(x):return x/atto
+def to_zepto(x):return x/zepto
+def to_yocto(x):return x/yocto
